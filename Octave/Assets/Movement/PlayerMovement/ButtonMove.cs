c3db27d39ec8181244a360abd06596f023997f3e
Meaghan Searle <msearle88@gmail.com>
2018-10-11 18:25:33 -0700

    trying to get the note to move the way I want it to

---
+++ Octave/Assets/Movement/PlayerMovement/ButtonMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ButtonMove : MonoBehaviour
+{
+	private float dirX, dirY;
+
+	[Range(1f, 20f)] public float moveSpeed = 3f;
+
+
+	private Rigidbody2D rb;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+
+		rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+		dirY = CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed * Time.deltaTime; 
+		
+		transform.position = new Vector2(transform.position.x + dirX, transform.position.y + dirY);
+
+	}
+}
---
+++ Octave/Assets/Movement/PlayerMovement/PlayerMovePartTwo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovePartTwo : MonoBehaviour
+{
+
+	float directionY;
+	[Range(1f, 20f)] public float moveSpeed = 3f; 
+	
+
+	void Update()
+	{
+		directionY = CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+	}
+}
