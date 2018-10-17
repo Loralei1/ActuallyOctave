d45c7a714feb16602ed0d229ce0068d211c486ee
Meaghan Searle <msearle88@gmail.com>
2018-10-17 13:43:36 -0700

    Still working on the score

--- Octave/Assets/GameStuff/Scripts/PlayerScore.cs
+++ Octave/Assets/GameStuff/Scripts/PlayerScore.cs
@@ -1,11 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
+
 {
+    private int scorenum;
+    public Text ScoreText;
+
+    private void Start()
+    {
+        scorenum = 0; 
+        SetScoreText();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        throw new System.NotImplementedException();
+        switch (tag)
+        {
+            case ("QuarterNote"):
+                scorenum = scorenum + 1;
+                SetScoreText();
+                    break; 
+            case ("HalfNote"):
+                scorenum = scorenum + 2; 
+                SetScoreText();
+                    break; 
+                    
+        }
+    }
+    
+    void SetScoreText()
+    {
+        ScoreText.text = "Score: " + scorenum.ToString();
     }
 }
