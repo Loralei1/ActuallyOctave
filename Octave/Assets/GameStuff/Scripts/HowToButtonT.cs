ee38c6ad8a6fcf758da1908e717f7195e984fd70
Meaghan Searle <msearle88@gmail.com>
2018-10-27 16:04:25 -0700

    Everything is good to go

---
+++ Octave/Assets/GameStuff/Scripts/HowToButtonT.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowToButtonT : MonoBehaviour {
+
+	public void HowToGo()
+	{
+		SceneManager.LoadScene(5);
+	}
+}
---
+++ Octave/Assets/GameStuff/Scripts/InfoTo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InfoTo : MonoBehaviour {
+
+	public void ToInfo ()
+	{
+		SceneManager.LoadScene(6);
+	}
+}
