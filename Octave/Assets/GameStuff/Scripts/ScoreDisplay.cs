427e13786f9723383845bfb9d438493d6323353c
Meaghan Searle <msearle88@gmail.com>
2018-10-27 14:35:57 -0700

    transition loop sound, deleted temp sounds

---
+++ Octave/Assets/GameStuff/Scripts/ScoreDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+
+	public Text ScoreText;
+	public IntData Cash;
+
+	private void Start()
+	{
+		ScoreText.text = "Current Score: " + Cash.Value.ToString();
+	}
+}
