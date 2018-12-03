300fa279c678aa4d65b00cc54693782b98ff9e7e
Meaghan Searle <msearle88@gmail.com>
2018-12-03 13:23:59 -0800

    play countdown scene

---
+++ Octave/Assets/GameStuff/Scripts/PlayStartText.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PlayStartText : MonoBehaviour
+{
+
+	public Text One;
+	public Text Two;
+	public Text Three;
+	public UnityEvent Event; 
+
+	IEnumerator Start()
+
+	{
+		SetOneText();
+		SetTwoText();
+		SetThreeText();
+		yield return new WaitForSeconds(2);
+		One.text = "1. Shoot notes to score points.";
+		//SetOneText();
+		yield return new WaitForSeconds(2);
+		Two.text = "2. Collide with rests to gain time.";
+		//SetTwoText();
+		yield return new WaitForSeconds(2);
+		Three.text = "3. Avoid fermatas.";
+		//SetThreeText();
+		yield return new WaitForSeconds(8);
+		Event.Invoke();
+		
+	}
+
+	private void SetOneText()
+	{
+		One.text = "";
+	}
+
+	private void SetTwoText()
+	{
+		Two.text = "";
+	}
+
+	private void SetThreeText()
+	{
+		Three.text = "";
+	}
+	
+	
+	/*private void Update()
+	{
+		SetOneText();
+		SetTwoText();
+		SetThreeText(); 
+	}*/
+}
--- Octave/Assets/GameStuff/Store/Store.cs
+++ Octave/Assets/GameStuff/Store/Store.cs
@@ -1,10 +1,12 @@
 ﻿using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "Store", menuName = "Store/StoreFront")]
 public class Store : ScriptableObject
@@ -13,8 +15,26 @@ public class Store : ScriptableObject
 	public Objects Available;
 	public Objects Purchased;
 	public IntData Cash;
-
+	public GameObject Button;
+	public GameObject Canvas;
+	
 	public UnityEvent MadePurchase;
+
+	/*public void BuildUI()
+	{
+		var newCanvas = Instantiate(Canvas);
+		var newButton = Instantiate(Button, newCanvas.GetComponentInChildren<VerticalLayoutGroup>().transform);
+		var buttonComponent = newButton.GetComponent<Button>();
+		buttonComponent.onClick.AddListener() => (MakePurchase(Purchased.ObjectList[0]);
+		var labe = newButton.GetComponentInChildren<Text>();
+		Label.text = Purchased.ObjectList[0].name; 
+
+	}
+
+	private void PurchaseButton();
+	{
+	}
+	*/
 
 	private void MakePurchase(PurchasableObject obj)
 	{
