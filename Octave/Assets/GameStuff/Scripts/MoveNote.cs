aba1ba7d87921abfd1bd5f4a7316d3f728644306
Meaghan Searle <msearle88@gmail.com>
2018-12-09 15:51:23 -0800

    changed a few script names

--- Octave/Assets/GameStuff/Scripts/MoveNote.cs
+++ Octave/Assets/GameStuff/Scripts/MoveNote.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mover : MonoBehaviour {
+public class MoveNote : MonoBehaviour {
 
 	private Rigidbody rb; 
 	public float Speed; 
--- Octave/Assets/GameStuff/Store/InstantObj.cs
+++ Octave/Assets/GameStuff/Store/InstantObj.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SetActive : MonoBehaviour
+public class InstantObj : MonoBehaviour
 {
 	public GameObject Test;
 	public Transform transform;
